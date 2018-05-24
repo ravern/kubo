cba5ab567e20274853a3b2066f33ba6062c2fdfc
Ravern Koh <ravernkoh@gmail.com>
2018-05-24 21:59:49 +0800

    fix some flag parsing issues, terminologies and add more comments

--- app.go
+++ app.go
@@ -29,14 +29,14 @@ func (a *App) Run(args []string) error {
 	for {
 		tmpArgs := args[1:]
 
-		// Verify command arguments
+		// Verify that multiple is only used once in the arguments
 		for i, arg := range cmd.Arguments {
 			if arg.Multiple && i != len(cmd.Arguments)-1 {
 				panic(fmt.Errorf("command %s: multiple can only be used in last argument", cmd.Name))
 			}
 		}
 
-		// Context to be passed to the command
+		// Create the context to pass to the command
 		ctx := Context{
 			arguments: make(map[string]string),
 			flags:     make(map[string]string),
@@ -44,44 +44,73 @@ func (a *App) Run(args []string) error {
 			stdout:    a.Stdout,
 		}
 
-		// Parse all the flags in the raw arguments
+		// flagErr is used to hold the flag not found error, which can
+		// only be returned if no subcommand is found
+		var flagErr error
+
+		// Parse all the flags in the arguments
 		for i := 0; i < len(tmpArgs); i++ {
 			arg := tmpArgs[i]
 
 			name, ok := parseFlagName(arg)
 			if ok {
+				// Try to find the flag definition
 				flag, err := cmd.flag(name)
 				if err != nil {
-					return err
+					// Since it is not find, hold the flag
+					// not found error for later and simply
+					// let it parse as per normal
+					flagErr = err
+					flag.Bool = false
 				}
 
 				var value string
 				if flag.Bool {
 					value = fmt.Sprint(true)
-					tmpArgs = append(tmpArgs[:i], tmpArgs[i+1:]...)
+					tmpArgs = append(append([]string{}, tmpArgs[:i]...), tmpArgs[i+1:]...)
 				} else if i+1 < len(tmpArgs) {
 					value = tmpArgs[i+1]
-					tmpArgs = append(tmpArgs[:i], tmpArgs[i+2:]...)
+					tmpArgs = append(append([]string{}, tmpArgs[:i]...), tmpArgs[i+2:]...)
 				} else {
 					return fmt.Errorf("no value found for flag: %s", name)
 				}
 				i-- // decrement to keep index correct
 
-				ctx.flags[name] = value
+				// Don't set the flag in the context since it
+				// was not defined in the commmand
+				if flagErr == nil {
+					ctx.flags[flag.Name] = value
+				}
 			}
 		}
 
-		// Parse as child command
+		// Parse raw arguments as child command
 		if len(tmpArgs) > 0 {
+			// Try to find child command
 			child, err := cmd.command(tmpArgs[0])
-			if err == nil {
+			if err != nil {
+				// If no child command is found and it not possibly
+				// an argument, then return the command not found
+				// error
+				if len(cmd.Arguments) == 0 {
+					return err
+				}
+			} else {
+				// Set the command to the child and pop the first
+				// argument
 				cmd = child
 				args = args[1:]
 				continue
 			}
 		}
 
-		// Parse as arguments
+		// Since no subcommand is found, the flag not found error should
+		// be returned if it is not nil
+		if flagErr != nil {
+			return flagErr
+		}
+
+		// Parse raw arguments as arguments
 		for _, arg := range cmd.Arguments {
 			if len(tmpArgs) == 0 {
 				return fmt.Errorf("argument not found: %s", arg.Name)
